988c8bf7979a2788978c76f8c29ec72099ea1952
Rodrigo Marques <rodrigomarques1993@gmail.com>
2023-10-06 09:50:33 -0300

    Small changes

--- app/decorators/paginating_decorator.rb
+++ app/decorators/paginating_decorator.rb
@@ -1,3 +1,7 @@
 class PaginatingDecorator < Draper::CollectionDecorator
-  delegate :current_page, :per_page, :offset, :total_entries, :total_pages
+  delegate :current_page,
+           :per_page,
+           :offset,
+           :total_entries,
+           :total_pages
 end
